a297536270387768bd90cd41aab088e65dbbb048
Wilson Bell <wilsontbell@gmail.com>
2022-07-08 10:19:47 -0500

    login and registration added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,19 @@
-import { GiffyGram } from "./GiffyGram.js"
+import { fetchUsers } from "./data/provider.js";
+import { LoginForm } from "./auth/Login.js";
+import { GiffyGram } from "./GiffyGram.js";
+import { RegisterForm } from "./auth/Register.js";
 
 const applicationElement = document.querySelector(".giffygram")
 
 export const renderApp = () => {
-    const user = parseInt(localStorage.getItem("gg_user"))
+    const user = (localStorage.getItem("gg_user"))
 
-    if (user) {
+    if (user != undefined) {
         applicationElement.innerHTML = GiffyGram()
     } else {
-        applicationElement.innerHTML = LoginForm()
+        applicationElement.innerHTML = LoginForm() + RegisterForm()
     }
 }
+
+fetchUsers()
+.then(renderApp())
