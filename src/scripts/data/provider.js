66d54115cfac8c6319819fc2d3fcfa0e9cd1318b
Christian Suriano <cratea113@gmail.com>
2022-07-08 13:46:19 -0500

    working on it

--- src/scripts/data/provider.js
+++ src/scripts/data/provider.js
@@ -3,6 +3,7 @@ const mainContainer = document.querySelector(".giffygram")
 
 
 const applicationState = {
+    messages: [],
     users: [],
     currentUser: {},
     feed: {
@@ -43,5 +44,46 @@ export const sendUser = (user) => {
 
 export const getUsers = () => {
     return applicationState.users.map(user => ({...user}))
+}
+
+export const sendMessage = (message) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(message)
+    }
+
+
+    return fetch(`${API}/messages`, fetchOptions)
+    .then(response => response.json())
+    .then(() => {
+        mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+    })
+}
+
+export const fetchMessage = () => {
+    return fetch(`${apiURL}/messages`)
+        .then(response => response.json())
+        .then(
+            (message) => {
+                // Store the external state in application state
+                applicationState.messages = message
+            }
+        )
+}
+
+export const deleteMessage = (id) => {
+    return fetch(`${API}/messages/${id}`, { method: "DELETE" })
+        .then(
+            () => {
+                mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+            }
+        )
+}
+
+export const getMessages = () => {
+    return applicationState.messages.map(message => ({...message}))
 }
 
--- src/scripts/message/MessageForm.js
+++ src/scripts/message/MessageForm.js
@@ -1,15 +1,23 @@
-import { getUsers } from "../data/provider.js"
+import { getUsers, getMessages, deleteMessage } from "../data/provider.js"
+
+mainContainer.addEventListener("click", click => {
+    if (event.target.id === "directMessageIcon") {
+
+
+        document.dispatchEvent(new CustomEvent("stateChanged"))
+    }
+})
 
 export const chooseRecipient = () => {
     const recipients = getUsers()
 
-    let html = "<h2>Recipient</h2>"
+    let html = "<h2>Recipient:</h2>"
 
     html += '<select id="recipient">'
-    html += '<option value="0">Choose a recipient</option>'
+    html += '<option value="0">Choose a recipient...</option>'
 
     const arrayOfOptions = recipients.map( (recipient) => {
-            return `<option value="${recipient.id}">${recipient.name}</option>`
+            return `<option value="messageRecipient--${recipient.id}">${recipient.id}</option>`
         }
     )
 
@@ -17,3 +25,34 @@ export const chooseRecipient = () => {
     html += "</select>"
     return html
 }
+
+export const recipientText = () => {
+    let html = `<h2>Message:</h2>
+    <div class="field">
+            <label class="label" for="userText">Message to User</label>
+            <input type="text" name="userText" class="input" />
+        </div>
+        
+        <button id="directMessage__submit" class="button">Save</button>
+        <button id="directMessage__cancel" class="button1">Cancel</button>
+        <button id="directMessage__close" class="button2">X</button>`
+        return html
+}
+
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "save") {
+        // Get what the user typed into the form fields
+        const text = document.querySelector("input[name='userText']").value
+        const recipientId = parseInt(document.querySelector("#recipient").value)
+        // Make an object out of the user input
+        const dataToSendToAPI = {
+            userText: text,
+            recipientId,    
+        }
+
+        // Send the data to the API for permanent storage
+        sendMessage(dataToSendToAPI)
+    }
+})
